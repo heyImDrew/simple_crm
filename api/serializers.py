2885ca006c61c1ad5674077469d973e5cff45852
Andrew <drew.forall@gmail.com>
2020-11-18 03:55:43 +0300

    [EDIT] pages [EDIT serializer type]

--- api/serializers.py
+++ api/serializers.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from employee.models import Employee
 from companies.models import Company, Partnership
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
 
-class CompanySerializer(serializers.ModelSerializer):
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
 
-class PartnershipSerializer(serializers.ModelSerializer):
+class PartnershipSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Partnership
         fields = '__all__'
